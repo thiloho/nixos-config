8a74ef1f3c9fe0576ba4500c15bef4f53afd310b
thiloho <123883702+thiloho@users.noreply.github.com>
2023-02-18 15:39:53 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nixos-config
+
+My personal NixOS configuration with flakes, home-manager as a module and multiple devices.
---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      mainpc = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos-configurations/mainpc
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+    };
+  };
+}
