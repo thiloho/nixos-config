dd7c7e874eb713d62dab95b694ed78bd613a6342
thiloho <123883702+thiloho@users.noreply.github.com>
2023-03-06 21:29:14 +0100

    Added flake input to include with nginx

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    website.url = "github:thiloho/thiloho.github.io";
   };
 
   outputs = inputs@{ nixpkgs, home-manager, ... }: {
@@ -31,6 +32,9 @@
         modules = [
           ./nixos-configurations/virtualserver
         ];
+        specialArgs = {
+          inherit (self) inputs;
+        };
       };
     };
   };
--- nixos-configurations/virtualserver/default.nix
+++ nixos-configurations/virtualserver/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -40,6 +40,7 @@
           enableACME = true;
           forceSSL = true;
           kTLS = true;
+          root = inputs.website.packages.${pkgs.stdenv.hostPlatform.system}.default;
           locations."/" = {
             proxyPass = "http://127.0.0.1:8000";
             proxyWebsockets = true;
