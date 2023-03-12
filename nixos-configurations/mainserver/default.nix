0953ff54bd9888952ceb6c1cbb06e2dc1ee2ea99
thiloho <123883702+thiloho@users.noreply.github.com>
2023-03-12 17:55:58 +0100

    Added wireguard configuration

--- nixos-configurations/mainserver/default.nix
+++ nixos-configurations/mainserver/default.nix
@@ -11,7 +11,7 @@
     mainserver-root-password.file = ../../secrets/mainserver-root-password.age;
     mainserver-thiloho-password.file = ../../secrets/mainserver-thiloho-password.age;
   #   mainserver-firefox-syncserver-secrets.file = ../../secrets/mainserver-firefox-syncserver-secrets.age;
-  #   mainserver-wireguard-private-key.file = ../../secrets/mainserver-wireguard-private-key.age;
+    mainserver-wireguard-private-key.file = ../../secrets/mainserver-wireguard-private-key.age;
   };
 
 
@@ -21,30 +21,30 @@
       allowedTCPPorts = [ 80 443 ];
       allowedUDPPorts = [ 51820 ];
     };
-    # nat = {
-    #   enable = true;
-    #   externalInterface = "eth0";
-    #   internalInterfaces = [ "wg0" ];
-    # };
-    # wireguard.interfaces = {
-    #   wg0 = {
-    #     ips = [ "10.100.0.1/24" ];
-    #     listenPort = 51820;
-    #     postSetup = ''
-    #       ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-    #     '';
-    #     postShutdown = ''
-    #       ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
-    #     '';
-    #     privateKeyFile = config.age.secrets.mainserver-wireguard-private-key.path;
-    #     peers = [
-    #       {
-    #         publicKey = "LCxf7Ca6aEn20rxDn6FiaGw3sdbwnhbi7FdW3dtf7SM=";
-    #         allowedIPs = [ "10.100.0.2/32" ];
-    #       }
-    #     ];
-    #   };
-    # };
+    nat = {
+      enable = true;
+      externalInterface = "eth0";
+      internalInterfaces = [ "wg0" ];
+    };
+    wireguard.interfaces = {
+      wg0 = {
+        ips = [ "10.100.0.1/24" ];
+        listenPort = 51820;
+        postSetup = ''
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        '';
+        postShutdown = ''
+          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+        '';
+        privateKeyFile = config.age.secrets.mainserver-wireguard-private-key.path;
+        peers = [
+          {
+            publicKey = "Gttz718cwmQlRKN/fMOEVl/PnCUOrWcM0ecChjPREw8=";
+            allowedIPs = [ "10.100.0.2/32" ];
+          }
+        ];
+      };
+    };
   };  
 
   users.users.root.passwordFile = config.age.secrets.mainserver-root-password.path;    
