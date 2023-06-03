8e0baa76734ab9c88c65d7fdcd0ebc4446c6f961
Thilo Hohlt <123883702+thiloho@users.noreply.github.com>
2023-06-03 06:10:37 +0200

    Update default.nix

--- nixos-configurations/pc/default.nix
+++ nixos-configurations/pc/default.nix
@@ -22,6 +22,10 @@
         modifier = "Mod1";
         terminal = "alacritty";
       };
+      extraConfig = ''
+        output DP-1 mode 1920x1080@144.000Hz
+        output DP-2 mode 1920x1080@144.000Hz
+      '';
     };
     programs.git = {
       signing = {
