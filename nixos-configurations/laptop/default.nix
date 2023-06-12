2fc82e37d173bc5ec025c939fa36ebf317c40705
thiloho <123883702+thiloho@users.noreply.github.com>
2023-06-12 22:19:58 +0200

    Update laptop configuration

--- nixos-configurations/laptop/default.nix
+++ nixos-configurations/laptop/default.nix
@@ -9,6 +9,16 @@
   networking.hostName = "laptop";
 
   home-manager.users.thiloho = { pkgs, ... }: {
+    wayland.windowManager.sway.config.output = let
+      wallpaper = pkgs.fetchurl {
+        url = "https://imgur.com/aAWzGqj.jpg";
+        hash = "sha256-iTQS939Zrvjv4rBJ6Y41kdsBBN9lT5yAoJnNg/WiMoA=";
+      };
+    in {
+      eDP-1 = {
+        bg = "${wallpaper} fill";
+      };
+    };
     programs.git = {
       signing = {
         key = "86C465C22C8A4D56";
