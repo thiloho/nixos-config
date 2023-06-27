b7ac1597dd108d435470c33f4de222a7cb53de65
thiloho <123883702+thiloho@users.noreply.github.com>
2023-06-27 19:24:13 +0200

    Add conveniene command for boot version history

--- nixos-configurations/shared.nix
+++ nixos-configurations/shared.nix
@@ -155,6 +155,7 @@
           rbs = "sudo nixos-rebuild switch --flake .";
           off = "sudo systemctl poweroff";
           cleanup = "nix store optimise && nix-collect-garbage -d && sudo nix store optimise && sudo nix-collect-garbage -d";
+          listboots = "nix profile history --profile /nix/var/nix/profiles/system";
         };
       };
       helix = {
