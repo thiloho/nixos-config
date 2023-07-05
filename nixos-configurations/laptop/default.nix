93126f0a9995201a374ae168b7f06a15becfc2c6
Thilo Hohlt <123883702+thiloho@users.noreply.github.com>
2023-07-05 23:09:21 +0200

    Adjust laptop config

--- nixos-configurations/laptop/default.nix
+++ nixos-configurations/laptop/default.nix
@@ -8,18 +8,8 @@
   ];
 
   networking.hostName = "laptop";
-  
-  hardware.firmware = [ pkgs.broadcom-bt-firmware ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
 
   home-manager.users.thiloho = { pkgs, ... }: {
-    wayland.windowManager.sway.config.output = let
-      wallpaper = pkgs.callPackage ../wallpaper.nix {};
-    in {
-      eDP-1 = {
-        bg = "${wallpaper} fill";
-      };
-    };
     programs.git = {
       signing = {
         key = "11BA77C2BDCEBF6A";
