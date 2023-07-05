7b2d2c5372b14396465fa820d8553014711134e2
Thilo Hohlt <123883702+thiloho@users.noreply.github.com>
2023-07-05 07:00:22 +0200

    Update shared-desktop.nix

--- nixos-configurations/shared-desktop.nix
+++ nixos-configurations/shared-desktop.nix
@@ -34,20 +34,20 @@
         picture-uri-dark = "${wallpaper}";
       };
     };
-    programs = {
-      gtk = {
-        enable = true;
-        gtk3.extraConfig = {
-          Settings = ''
-            gtk-application-prefer-dark-theme=1
-          '';
-        };
-        gtk4.extraConfig = {
-          Settings = ''
-            gtk-application-prefer-dark-theme=1
-          '';
-        };
+    gtk = {
+      enable = true;
+      gtk3.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
       };
+      gtk4.extraConfig = {
+        Settings = ''
+          gtk-application-prefer-dark-theme=1
+        '';
+      };
+    };
+    programs = {
       bash = {
         enable = true;
         shellAliases = {
