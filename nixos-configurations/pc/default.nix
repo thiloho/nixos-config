d23dc3f8ab26eceef8db65abd33f51fbade7554e
Thilo Hohlt <123883702+thiloho@users.noreply.github.com>
2023-07-05 05:58:00 +0200

    Update default.nix

--- nixos-configurations/pc/default.nix
+++ nixos-configurations/pc/default.nix
@@ -10,18 +10,6 @@
   networking.hostName = "pc";
 
   home-manager.users.thiloho = { pkgs, lib, ... }: {
-    wayland.windowManager.sway.config.output = let
-      wallpaper = pkgs.callPackage ../wallpaper.nix {};
-    in {
-      DP-1 = {
-        bg = "${wallpaper} fill";
-        res = "1920x1080@144.000Hz";
-      };
-      DP-2 = {
-        bg = "${wallpaper} fill";
-        res = "1920x1080@144.000Hz";
-      };
-    };
     programs.git = {
       signing = {
         key = "A6C6D25B9687377B";
