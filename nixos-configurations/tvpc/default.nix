52d805ec58431135b073a84a09ff32d798b71e8c
Thilo Hohlt <123883702+thiloho@users.noreply.github.com>
2023-08-25 09:40:45 +0200

    Update default.nix

--- nixos-configurations/tvpc/default.nix
+++ nixos-configurations/tvpc/default.nix
@@ -10,6 +10,7 @@
   networking.hostName = "tvpc";
 
   home-manager.users.thiloho = { pkgs, lib, ... }: {
+    programs.git.signing.key = "5ECD00BDC15A987E";
     home.stateVersion = "23.05";
   };
   system.stateVersion = "23.05";
