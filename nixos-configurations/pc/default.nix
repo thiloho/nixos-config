88874ec6c51ac989d2ebde70462da5d71687d9e4
thiloho <123883702+thiloho@users.noreply.github.com>
2023-08-29 16:03:38 +0200

    Add test postgres environment for pc

--- nixos-configurations/pc/default.nix
+++ nixos-configurations/pc/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -8,6 +8,16 @@
   ];
 
   networking.hostName = "pc";
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_15;
+    ensureDatabases = [ "dcbot" ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser auth-method
+      local all      all    trust
+    '';
+  };
 
   home-manager.users.thiloho = { pkgs, lib, ... }: {
     programs.git.signing.key = "5ECD00BDC15A987E";
