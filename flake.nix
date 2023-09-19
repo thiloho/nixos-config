ebd5e6a1a7b24a8e322da8d65f55a5656abf6b82
Thilo Hohlt <123883702+thiloho@users.noreply.github.com>
2023-09-19 14:44:43 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -33,6 +34,7 @@
           }
           entrypoint
           home-manager.nixosModules.home-manager
+          nixos-hardware.nixosModules.lenovo-thinkpad-t480
           {
             home-manager = {
               useGlobalPkgs = true;
