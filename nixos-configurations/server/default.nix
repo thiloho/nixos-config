3638553b88bf12962f92da61b62dd67d53168c69
thiloho <123883702+thiloho@users.noreply.github.com>
2023-11-26 14:34:21 +0100

    Update flake

--- nixos-configurations/server/default.nix
+++ nixos-configurations/server/default.nix
@@ -127,6 +127,8 @@
       enable = true;
       package = pkgs.postgresql_15;
       ensureDatabases = [ "dcbot" "hedgedoc" "todos" "gitea" ];
+      # ALTER DATABASE mydb OWNER TO admin;
+      # The user also needs to be the owner of the specific database, which cannot be declaratively set up from this module
       ensureUsers = [
         {
           name = "hedgedoc";
