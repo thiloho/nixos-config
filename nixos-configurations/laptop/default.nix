819b6026d2d78ce5f3a3ed4d783d785714f136ca
thiloho <123883702+thiloho@users.noreply.github.com>
2024-05-05 00:36:43 +0200

    Add mariadb

--- nixos-configurations/laptop/default.nix
+++ nixos-configurations/laptop/default.nix
@@ -10,6 +10,12 @@
 
   networking.hostName = "laptop";
 
+
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
   home-manager.users.thiloho = { pkgs, ... }: {
     programs.git.signing.key = "BFD8F6A55B1E4F11";
     home = { stateVersion = "23.05"; };
