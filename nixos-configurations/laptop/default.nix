7e9a2d6b30d2de15fe48d7bd13f69d36cf8241cb
thiloho <123883702+thiloho@users.noreply.github.com>
2024-06-24 17:07:19 +0200

    Add config for laptop GDM login screen

--- nixos-configurations/laptop/default.nix
+++ nixos-configurations/laptop/default.nix
@@ -10,11 +10,37 @@
 
   networking.hostName = "laptop";
 
-
   services.mysql = {
     enable = true;
     package = pkgs.mariadb;
   };
+
+  systemd.tmpfiles.rules = [
+    "L+ /run/gdm/.config/monitors.xml - - - - ${pkgs.writeText "gdm-monitors.xml" ''
+      <monitors version="2">
+        <configuration>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>1</scale>
+            <monitor>
+              <monitorspec>
+                <connector>eDP-1</connector>
+                <vendor>BOE</vendor>
+                <product>0x074f</product>
+                <serial>0x00000000</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.012</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
+      </monitors>
+    ''}"
+  ];
 
   home-manager.users.thiloho = { pkgs, ... }: {
     programs.git.signing.key = "5CF074CA31DD6686";
