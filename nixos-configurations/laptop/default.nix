32060797a114ef89e189513b8c8556cb7564ccb5
thiloho <123883702+thiloho@users.noreply.github.com>
2024-08-06 14:04:28 +0200

    Revert "Change to Plasma and remove unnecessary packages"
    This reverts commit 90625b6837ff01578229289263d6bf4ebcbdc497.

--- nixos-configurations/laptop/default.nix
+++ nixos-configurations/laptop/default.nix
@@ -15,6 +15,33 @@
     package = pkgs.mariadb;
   };
 
+  systemd.tmpfiles.rules = [
+    "L+ /run/gdm/.config/monitors.xml - - - - ${pkgs.writeText "gdm-monitors.xml" ''
+      <monitors version="2">
+        <configuration>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>1</scale>
+            <monitor>
+              <monitorspec>
+                <connector>eDP-1</connector>
+                <vendor>BOE</vendor>
+                <product>0x074f</product>
+                <serial>0x00000000</serial>
+              </monitorspec>
+              <mode>
+                <width>1920</width>
+                <height>1080</height>
+                <rate>60.012</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
+      </monitors>
+    ''}"
+  ];
+
   home-manager.users.thiloho = { pkgs, ... }: {
     programs.git.signing.key = "5CF074CA31DD6686";
     home = { stateVersion = "23.05"; };
--- nixos-configurations/shared-desktop.nix
+++ nixos-configurations/shared-desktop.nix
@@ -4,14 +4,18 @@
   services = {
     xserver = {
       enable = true;
-    };
-    displayManager = {
-      sddm = {
+      displayManager.gdm.enable = true;
+      desktopManager.gnome = {
         enable = true;
-        wayland.enable = true;
+        # extraGSettingsOverridePackages = [ pkgs.gnome.mutter ];
+        # fractional scaling support
+        # extraGSettingsOverrides = ''
+        #   [org.gnome.mutter]
+        #   experimental-features=['scale-monitor-framebuffer']
+        # '';
       };
+      excludePackages = [ pkgs.xterm ];
     };
-    desktopManager.plasma6.enable = true;
     pipewire = {
       enable = true;
       alsa.enable = true;
@@ -28,6 +32,7 @@
     postgresql = {
       enable = true;
       package = pkgs.postgresql_15;
+      ensureDatabases = [ "dcbot" "todos" ];
       authentication = lib.mkForce ''
         local all all trust
         host all all 0.0.0.0/0 scram-sha-256
@@ -35,11 +40,6 @@
       '';
     };
   };
-
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    plasma-browser-integration
-    kate
-  ];
 
   users = {
     mutableUsers = false;
@@ -76,6 +76,7 @@
   };
 
   hardware.pulseaudio.enable = false;
+
   hardware.bluetooth.enable = true;
 
   home-manager.users.thiloho = { pkgs, lib, config, ... }: {
@@ -112,7 +113,6 @@
           }
         ];
         userSettings = {
-          "window.titleBarStyle" = "custom";
           "nix.enableLanguageServer" = true;
           "nix.serverPath" = "nil";
           "editor.indentSize" = 2;
@@ -142,17 +142,28 @@
     home = {
       sessionVariables = { NIXOS_OZONE_WL = 1; };
       packages = with pkgs; [
+        libreoffice
+        airshipper
         tldr
         prismlauncher
         ventoy-full
+        psensor
         nil
         zoom-us
         qbittorrent
         neofetch
+        godot_4
         backblaze-b2
         localsend
+        mullvad-browser
         postman
+        dbeaver-bin
+        texliveFull
+        gnome-tweaks
+        gnome-themes-extra
         melonDS
+        amberol
+        zed-editor
       ];
     };
   };
